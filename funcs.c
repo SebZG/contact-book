dc0d9092df359ff6deac26181a73c5213696ccfa
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2025-11-25 20:31:23 +0000

    Add: function definitions

---
+++ funcs.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "funcs.h"
+
+void trimNewLine(char *str) {
+  int len = strlen(str);
+  if (len > 0 && str[len - 1] == '\n')
+    str[len - 1] = '\0';
+}
+
+int isValidPhone(const char *phone) {
+  int len = strlen(phone);
+
+  if (len == 0)
+    return 0;
+
+  // Remove newline if present (for fgets)
+  if (phone[len - 1] == '\n')
+    len--;
+
+  // Lengh must be between 7 and 14
+  if (len < 7 && len > 14)
+    return 0;
+
+  // Must contain only digits
+  for (int i = 0; i < len; i++)
+    if (phone[i] < '0' || phone[i] > '9')
+      return 0;
+
+  return 1;
+}
+
+void addContact(void) {
+  if (count >= MAX) {
+    printf("Contact book is full!\n");
+    return;
+  }
+
+  printf("Enter name: ");
+  if (fgets(names[count], sizeof(names[count]), stdin) == NULL) {
+    printf("Failed to read name.\n");
+    return;
+  }
+  trimNewLine(names[count]);
+
+  while (1) {
+    printf("Enter phone number: ");
+    if (fgets(phones[count], sizeof(phones[count]), stdin) == NULL) {
+      printf("Failed to read phone number.\n");
+      return;
+    }
+    trimNewLine(phones[count]);
+
+    if (isValidPhone(phones[count]))
+      break; // Valid number;
+    else
+      printf("Invalid phone number! It must be between 7-14 digits and contain "
+             "only numbers.\n");
+  }
+
+  printf("Contact added successfully.\n");
+  count++;
+}
+
+void displayContacts(void) {
+  if (count == 0) {
+    printf("No contacts to display.\n");
+    return;
+  }
+
+  printf("\n--- Contact List ---\n");
+  for (int i = 0; i < count; i++)
+    printf("%d) Name: %s, Phone: %s\n", i + 1, names[i], phones[i]);
+}
+
+int byName(int index, const char *query) {
+  if (index == count)
+    return -1;
+
+  if (strcmp(names[count], query) == 0)
+    return index;
+
+  return byName(index + 1, query);
+}
+
+int byPhone(int index, const char *query) {
+  if (index == count)
+    return -1;
+
+  if (strcmp(names[count], query) == 0)
+    return index;
+
+  return byPhone(index + 1, query);
+}
+
+int search(int (*op)(int, const char *), int index, const char *query) {
+  return op(index, query);
+}
+
+void deleteContact(void) {
+  char query[NAME_LEN];
+  int index;
+
+  printf("Enter the name to delete: ");
+  if (fgets(query, sizeof(query), stdin) == NULL) {
+    printf("Failed to read input.\n");
+    return;
+  }
+  trimNewLine(query);
+
+  index = search(byName, index, query);
+  if (index == -1) {
+    printf("Contact not found.\n");
+    return;
+  }
+
+  // If it's the last contact, just reduce count
+  if (index == count - 1)
+    count--;
+  else { // Copy last to correct index and reduce count
+    strcpy(names[index], names[count - 1]);
+    strcpy(phones[index], phones[count - 1]);
+    count--;
+  }
+
+  printf("Contact deleted successfully.\n");
+}
--- main.c
+++ main.c
@@ -2,12 +2,19 @@
 
 #include "funcs.h"
 
+/*
+    names[0][{"Sebastian"}]
+    names[1][{"John"}]
+    names[2][{"Michael"}]
+
+    phones[0][{"1234567"}]
+    phones[1][{"1234567"}]
+    phones[2][{"1234567"}]
+*/
+
 // Globals (storage for contacts)
 char names[MAX][NAME_LEN];
 char phones[MAX][PHONE_LEN];
 int count = 0;
 
-int main() {
-
-    return 0;
-}
+int main() { return 0; }
